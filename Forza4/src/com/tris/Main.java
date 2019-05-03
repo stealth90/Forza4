e4e6159356dcc6da7ff18fa3e3195ec826cc3bc0
Pierantonio Petralia <41144486+stealth90@users.noreply.github.com>
2019-05-03 09:19:15 +0200

    Update Main.java

--- Forza4/src/com/tris/Main.java
+++ Forza4/src/com/tris/Main.java
@@ -3,13 +3,5 @@ package com.tris;
 public class Main {
 
     public static void main(String[] args) {
-    Grid a = new Grid();
-    String gameTurn;
-    gameTurn=a.setSign(5,1);
-        System.out.println(gameTurn);
-        gameTurn=a.setSign(4,5);
-        System.out.println(gameTurn);
-        gameTurn=a.setSign(2,1);
-        System.out.println(gameTurn);
     }
 }
